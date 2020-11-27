7011f0ff273f5c9927dfd3707e6fe1cd3b56e6d6
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2020-11-27 11:55:48 +0100

    ArchiveView - Adjust component to be fetch-ready

--- src/components/ArchiveResults/ArchiveResults.js
+++ src/components/ArchiveResults/ArchiveResults.js
@@ -4,16 +4,41 @@ import ArchiveResultsHeader from './ArchiveResultsHeader/ArchiveResultsHeader';
 import { ResultsTable } from './StyledArchiveResults';
 
 const ArchiveResults = () => {
+  const archiveResults = [
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+    { id: '1234/2020', date: '2020-08-12', configuration: '1+3', engineer: 'Jan Kowalski', technician: 'Tomasz Nowak' },
+  ];
   return (
     <ResultsTable>
       <ArchiveResultsHeader />
-      <ArchiveResult
-        id={'1234/2020'}
-        date={'2020-08-12'}
-        configuration={'1+3'}
-        engineer={'Jan Kowalski'}
-        technician={'Tomasz Nowak'}
-      />
+      {archiveResults.map((result) => (
+        <ArchiveResult
+          id={result.id}
+          date={result.date}
+          configuration={result.configuration}
+          engineer={result.engineer}
+          technician={result.technician}
+        />
+      ))}
     </ResultsTable>
   );
 };
