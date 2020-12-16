d95cb5b1a73da50bb3a91c5891b0a87959065164
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2020-12-16 08:26:51 +0100

    Exchange Report - Add Exchange report component

--- src/Pages/NewExchangeReport/ExchangeReportForm/StyledExchangeReportForm.js
+++ src/Pages/NewExchangeReport/ExchangeReportForm/StyledExchangeReportForm.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
-export const StyledExchangeReportForm = styled.form`
+export const StyledExchangeReportForm = styled.div`
   width: 100%;
-  height: 100%;
+  height: 60%;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
--- src/Pages/NewExchangeReport/NewExchangeReport.js
+++ src/Pages/NewExchangeReport/NewExchangeReport.js
@@ -1,7 +1,6 @@
 import React from 'react';
-
 import { MainHeader } from '../../components/UI/Headers/Headers';
-
+import SeparateLine from '../../components/UI/SeparateLine/SeparateLine';
 import ExchangeReportForm from './ExchangeReportForm/ExchangeReportForm';
 
 const NewExchangeReport = () => {
@@ -9,6 +8,7 @@ const NewExchangeReport = () => {
     <>
       <MainHeader>Formularz raportu wymiany</MainHeader>
       <ExchangeReportForm />
+      <SeparateLine />
     </>
   );
 };
