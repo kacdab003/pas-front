ca110512aea5640d00d8a5655c5f0aa6ba7e695a
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-02 14:41:05 +0100

    FE-8 Redux store - Add redux store and its actions

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 import MainView from './Pages/MainView/MainView';
 import Layout from './containers/Layout';
-import { Route } from 'react-router';
+import { Redirect, Route } from 'react-router';
 import NewReport from './Pages/NewReport/NewReport';
 import Archive from './Pages/Archive/Archive';
 import NewExchangeReport from './Pages/NewExchangeReport/NewExchangeReport';
@@ -10,9 +10,6 @@ import NewExchangeReport from './Pages/NewExchangeReport/NewExchangeReport';
 const App = () => {
   return (
     <Layout>
-      <Route path={'/'} exact>
-        <MainView />
-      </Route>
       <Route path={'/new-report'}>
         <NewReport />
       </Route>
@@ -21,6 +18,12 @@ const App = () => {
       </Route>
       <Route path={'/archive'}>
         <Archive />
+      </Route>
+      <Route path={'/logout'}>
+        <Redirect to={'/'} />
+      </Route>
+      <Route path={'/'} exact>
+        <MainView />
       </Route>
     </Layout>
   );
---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,59 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false,
+  fullName: '',
+};
+
+const authStart = (state, action) => {
+  return {
+    ...state,
+    loading: true,
+    error: null,
+  };
+};
+const authSuccess = (state, action) => {
+  return {
+    ...state,
+    token: action.idToken,
+    userId: action.userId,
+    fullName: action.fullName,
+    error: null,
+    loading: false,
+  };
+};
+
+const authFail = (state, action) => {
+  return {
+    ...state,
+    error: action.error,
+    loading: false,
+  };
+};
+
+const authLogout = (state, action) => {
+  return {
+    ...state,
+    token: null,
+    userId: null,
+  };
+};
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS:
+      return authSuccess(state, action);
+    case actionTypes.AUTH_FAIL:
+      return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT:
+      return authLogout(state, action);
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
