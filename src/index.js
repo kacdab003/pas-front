b94d8f1942297c40e7715883213c367933d10b28
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-02 14:41:05 +0100

    FE-8 Redux store - Add redux store and its actions

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import authReducer from './store/reducers/authReducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/actions/auth.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import urls from '../../shared/urls';
+import * as actionTypes from './actionsTypes';
+
+const authStart = () => {
+  return { type: actionTypes.AUTH_START };
+};
+
+const authSuccess = (token, userId, fullName) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId,
+    fullName,
+  };
+};
+
+const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error,
+  };
+};
+
+export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
+  localStorage.removeItem('fullName');
+
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
+
+const checkTimeout = (expirationTime) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, Number(expirationTime * 1000));
+  };
+};
+
+export const auth = (login, password, onSuccess) => {
+  return async (dispatch) => {
+    dispatch(authStart());
+    try {
+      const authData = { username: login, password };
+      const response = await axios.post(urls.api + urls.login, authData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const { token: idToken, userId, fullName, expiresIn } = response.data;
+      if (!idToken || !userId) {
+        throw new Error('Unable to authenticate');
+      }
+      const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+      localStorage.setItem('token', idToken);
+      localStorage.setItem('expirationDate', expirationDate);
+      localStorage.setItem('fullName', fullName);
+      localStorage.setItem('userId', userId);
+      dispatch(authSuccess(idToken, userId, fullName));
+      dispatch(checkTimeout(expiresIn));
+
+      if (onSuccess) {
+        onSuccess();
+      }
+    } catch (error) {
+      const displayedError = error?.response?.data?.details || error?.message;
+
+      dispatch(authFail(displayedError));
+    }
+  };
+};
+
+export const authCheckState = () => {
+  return (dispatch) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return dispatch(logout());
+    }
+    const userId = localStorage.getItem('userId');
+    const fullName = localStorage.getItem('fullName');
+
+    const expirationDate = new Date(localStorage.getItem('expirationDate'));
+    const isTokenValid = expirationDate.getTime() >= new Date().getTime();
+    if (isTokenValid) {
+      dispatch(authSuccess(token, userId, fullName));
+      return dispatch(checkTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+    }
+
+    dispatch(logout());
+  };
+};
