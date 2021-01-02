ce1621ea2edb024bf6c797129848cde685b01c68
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-02 14:29:44 +0100

    FE-4 - Hide unnecessary navbar presence when it is closed

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import LoginSection from '../../AuthSection/AuthSection';
+import AuthSection from '../../AuthSection/AuthSection';
 import NavbarHeader from './NavbarHeader/NavbarHeader';
 import NavbarItems from './NavbarItems/NavbarItems';
 import { StyledNavbar } from './StyledNavbar';
 const Navbar = ({ isOpen, onToggle, isAuthenticated }) => {
-  console.log(isAuthenticated);
+  let navbarContent;
+  if (isOpen) {
+    navbarContent = !isAuthenticated ? <NavbarItems onToggle={onToggle} /> : <AuthSection />;
+  }
   return (
     <StyledNavbar isOpen={isOpen}>
       <NavbarHeader isOpen={isOpen} onToggle={onToggle} title={'TITLE'} />
-      {isAuthenticated ? <NavbarItems onToggle={onToggle} /> : <LoginSection />}
+      {navbarContent}
     </StyledNavbar>
   );
 };
