725daea2a1ced3efa5fdff5f9b5d3fc4b6b49402
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-06 19:00:36 +0100

    FE-4 - Fix navbar rendering

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import LoginSection from '../../AuthSection/AuthSection';
+import AuthSection from '../../AuthSection/AuthSection';
 import NavbarHeader from './NavbarHeader/NavbarHeader';
 import NavbarItems from './NavbarItems/NavbarItems';
 import { StyledNavbar } from './StyledNavbar';
 const Navbar = ({ isOpen, onToggle, isAuthenticated }) => {
-  console.log(isAuthenticated);
+  const navbarContent = isAuthenticated ? <NavbarItems onToggle={onToggle} /> : <AuthSection />;
   return (
     <StyledNavbar isOpen={isOpen}>
       <NavbarHeader isOpen={isOpen} onToggle={onToggle} title={'TITLE'} />
-      {isAuthenticated ? <NavbarItems onToggle={onToggle} /> : <LoginSection />}
+      {isOpen && navbarContent}
     </StyledNavbar>
   );
 };
