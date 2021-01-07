f42887f5fdab7228c89f00df8b8058d2e9301082
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-07 11:19:58 +0100

    FE-6 - Adjust try/catch blocks and add implicit return instead of else/if

--- src/components/ArchiveResults/ArchiveResults.js
+++ src/components/ArchiveResults/ArchiveResults.js
@@ -5,6 +5,7 @@ import ArchiveResult from './ArchiveResult/ArchiveResult';
 import ArchiveResultsHeader from './ArchiveResultsHeader/ArchiveResultsHeader';
 import { ResultsTable } from './StyledArchiveResults';
 import moment from 'moment';
+import { archiveEndpoints } from '../../shared/config/endpoints';
 
 const ArchiveResults = () => {
   const [archiveResults, setArchiveResults] = useState([]);
@@ -13,29 +14,31 @@ const ArchiveResults = () => {
 
   useEffect(() => {
     const fetchArchiveResults = async () => {
-      await Axios.get('http://localhost:3000/reports')
-        .then((response) => {
-          const mappedResults = response.data.map((result) => ({
-            key: result._id,
-            id: result.nr,
-            date: moment(result.createdAt).format('YYYY-MM-DD hh:mm'),
-            configuration: result.configuration,
-            fullName: result.worker.name + ' ' + result.worker.surname,
-            office: result.worker.position === 'ENGINEER' ? 'INŻYNIER' : 'TECHNIK',
-          }));
-          setArchiveResults(mappedResults);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          setError(error.response.data);
-          setIsLoading(false);
-        });
+      try {
+        const response = await Axios.get(archiveEndpoints.get);
+
+        const mappedResults = response.data.map((result) => ({
+          key: result._id,
+          id: result.nr,
+          date: moment(result.createdAt).format('YYYY-MM-DD hh:mm'),
+          configuration: result.configuration,
+          fullName: result.worker.name + ' ' + result.worker.surname,
+          office: result.worker.position === 'ENGINEER' ? 'INŻYNIER' : 'TECHNIK',
+        }));
+        setArchiveResults(mappedResults);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.response.data);
+        setIsLoading(false);
+      }
     };
+
     fetchArchiveResults();
   }, []);
   if (isLoading) {
     return <CenteredLoader />;
-  } else if (error) {
+  }
+  if (error) {
     return <div style={{ textAlign: 'center', fontWeight: 'bold', color: 'red' }}>BŁĄD WCZYTYWANIA DANYCH!</div>;
   }
   return (
