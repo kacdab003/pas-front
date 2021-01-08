b01884a9317a6fa65dfc4dd31aa0818498b4b86f
Karol <lokarcqaz@gmail.com>
2021-01-08 16:40:08 +0100

    Initial select implement

--- src/components/FormSelect/FormSelect.js
+++ src/components/FormSelect/FormSelect.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyledFormSelect, StyledFormLabel, StyledSelectWrapper } from './StyledFormSelect';
 
-const FormSelect = ({ labelContent, selectProps }) => {
+const FormSelect = ({ labelContent, selectProps, children }) => {
   return (
     <StyledSelectWrapper>
       <StyledFormLabel>{labelContent}</StyledFormLabel>
-      <StyledFormSelect {...selectProps} />
+      <StyledFormSelect {...selectProps}>{children}</StyledFormSelect>
     </StyledSelectWrapper>
   );
 };
--- src/shared/config/forms/exchangeReport.js
+++ src/shared/config/forms/exchangeReport.js
@@ -1,3 +1,5 @@
+import { userEndpoints, moduleEndpoints } from '../endpoints';
+
 const exchangeReportConfig = {
   inputs: [
     {
@@ -13,16 +15,24 @@ const exchangeReportConfig = {
     {
       label: 'Pracownik dokonujący wymiany',
       name: 'exchangeWorker',
-      placeholder: 'Imię i nazwisko pracownika',
       required: true,
+      refEndpoint: userEndpoints.get,
+      refKey: 'surname',
     },
     {
       label: 'Nr ewidencyjny uszkodzonego modułu',
       name: 'damagedModule',
-      placeholder: '',
       required: true,
+      refEndpoint: moduleEndpoints.get,
+      refKey: 'moduleNumber',
     },
-    { label: 'Nr ewidencyjny nowego modułu', name: 'newModule', placeholder: '', required: true },
+    {
+      label: 'Nr ewidencyjny nowego modułu',
+      name: 'newModule',
+      required: true,
+      refEndpoint: moduleEndpoints.get,
+      refKey: 'moduleNumber',
+    },
   ],
 };
 
