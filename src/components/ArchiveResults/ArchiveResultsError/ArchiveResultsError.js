c154339443215cd308edfe1faa27984f6822c59e
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-08 18:42:31 +0100

    FE-9 - Use new ErrorMessage component in archive view

--- src/components/ArchiveResults/ArchiveResultsError/ArchiveResultsError.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyledArchiveResultsError } from './StyledArchiveResultsError';
-const ArchiveResultsError = ({ message }) => {
-  console.warn(message);
-  return <StyledArchiveResultsError>{message}</StyledArchiveResultsError>;
-};
-
-export default ArchiveResultsError;
--- src/components/ArchiveResults/ArchiveResultsError/StyledArchiveResultsError.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledArchiveResultsError = styled.p`
-  text-align: center;
-  font-weight: bold;
-  color: red;
-`;
