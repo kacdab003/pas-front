6165760c0765586e384910d30859a7a5478b6b92
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-15 20:04:31 +0100

    FE-34 - Use useRequest hook in FormSelect

--- src/components/FormSelect/FormSelect.js
+++ src/components/FormSelect/FormSelect.js
@@ -1,41 +1,42 @@
-import axios from '../../axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyledFormSelect, StyledFormLabel, StyledSelectWrapper } from './StyledFormSelect';
+import useRequest, { REQUEST_METHODS } from '../../hooks/useRequest';
+import Message from '../Message/Message';
+import CenteredLoader from '../UI/CenteredLoader/CenteredLoader';
 
 const FormSelect = ({ defaultOptionLabel, labelContent, selectProps, refEndpoint, refKey, onError }) => {
-  const [options, setOptions] = useState([]);
-  useEffect(() => {
-    const fetchOptions = async () => {
-      try {
-        const response = await axios.get(refEndpoint);
+  const [options, optionsError, optionsLoading] = useRequest(refEndpoint, null, REQUEST_METHODS.GET);
 
-        const options = response.data.map((op) => {
-          return (
-            <option key={op._id} value={op._id}>
-              {op[refKey]}
-            </option>
-          );
-        });
-        const defaultOption = (
-          <option value="" key={'DEFAULT'} selected disabled>
-            {defaultOptionLabel}
-          </option>
-        );
-        options.unshift(defaultOption);
-        setOptions(options);
-      } catch (error) {
-        onError(error);
-      }
-    };
-    fetchOptions();
-  }, [onError, refKey, refEndpoint, defaultOptionLabel]);
+  const generateOptionsWithDefault = () => {
+    const optionsElements = options?.data?.map?.((op) => (
+      <option key={op._id} value={op._id}>
+        {op[refKey]}{' '}
+      </option>
+    ));
+    const defaultOption = (
+      <option value="" key={'DEFAULT'} selected disabled>
+        {defaultOptionLabel}
+      </option>
+    );
+    optionsElements?.unshift?.(defaultOption);
+    return optionsElements;
+  };
 
-  return (
-    <StyledSelectWrapper>
+  let content = (
+    <>
       <StyledFormLabel>{labelContent}</StyledFormLabel>
-      <StyledFormSelect {...selectProps}>{options}</StyledFormSelect>
-    </StyledSelectWrapper>
+      <StyledFormSelect {...selectProps}>{generateOptionsWithDefault()}</StyledFormSelect>
+    </>
   );
+
+  if (optionsError) {
+    content = <Message messageType={'ERROR'} />;
+  }
+  if (optionsLoading) {
+    content = <CenteredLoader color={'black'} />;
+  }
+
+  return <StyledSelectWrapper>{content}</StyledSelectWrapper>;
 };
 
 export default FormSelect;
