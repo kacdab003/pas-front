60d148595298e73d805df513b6361675631a5504
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-20 15:27:01 +0100

    Search - Add search in archive

--- src/components/ArchiveResults/ExchangeReports/ExchangeReports.js
+++ src/components/ArchiveResults/ExchangeReports/ExchangeReports.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Message from '../../Message/Message';
 import SearchBar from '../../SearchBar/SearchBar';
+import { ErrorResults } from '../StyledArchiveResults';
 import ExchangeReport from './ExchangeReport/ExchangeReport';
 
 const ExchangeReports = ({ exchangeReportsArray }) => {
-  const mappedExchangeReports = exchangeReportsArray.data.map((exchangeReport) => (
-    <ExchangeReport {...exchangeReport} />
-  ));
+  const [searchQuery, setSearchQuery] = useState('');
+
+  let mappedExchangeReports = exchangeReportsArray.data;
+  if (searchQuery) {
+    mappedExchangeReports = mappedExchangeReports.filter((report) => {
+      const isSearchResult =
+        report?.damagedModuleNumber?.toString?.()?.includes?.(searchQuery) ||
+        report?.exchangeDate?.toString?.()?.includes?.(searchQuery) ||
+        report?.exchangeWorker?.fullName?.includes?.(searchQuery) ||
+        report?.newModuleNumber?.toString?.()?.includes?.(searchQuery) ||
+        report?.objectNumber?.toString?.()?.includes?.(searchQuery) ||
+        report?.socker?.toString?.().includes?.(searchQuery);
+      return isSearchResult;
+    });
+  }
+  const exchangeReportsToRender = mappedExchangeReports.map((exchangeReport) => <ExchangeReport {...exchangeReport} />);
+  let content =
+    exchangeReportsToRender.length === 0 ? (
+      <ErrorResults>
+        <Message messageType={'ERROR'} message={'Brak wyników'} />
+      </ErrorResults>
+    ) : (
+      exchangeReportsToRender
+    );
   return (
     <>
-      <SearchBar label={'WYSZUKAJ'} placeholder={'Wpisz wyszukiwaną frazę'} />
-      {mappedExchangeReports}
+      <SearchBar
+        setValue={(event) => setSearchQuery(event.target.value)}
+        value={searchQuery}
+        label={'WYSZUKAJ'}
+        placeholder={'Wpisz wyszukiwaną frazę'}
+      />
+      {content}
     </>
   );
 };
--- src/components/ArchiveResults/Reports/Reports.js
+++ src/components/ArchiveResults/Reports/Reports.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import Report from './Report/Report';
 import { ReportsTable } from './StyledReports';
 import ReportHeader from '../ArchiveResultsHeader/ArchiveResultsHeader';
 import SearchBar from '../../SearchBar/SearchBar';
+import Message from '../../Message/Message';
+import { ErrorResults } from '../StyledArchiveResults';
 
 const Reports = ({ reportsArray }) => {
-  const mappedReportsArray = reportsArray.data.map((result) => ({
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const reportsArrayData = reportsArray.data;
+
+  let mappedReportsElements = reportsArrayData.map((result) => ({
     key: result._id,
     id: result.nr,
     date: moment(result.createdAt).format('YYYY-MM-DD hh:mm'),
@@ -14,15 +20,41 @@ const Reports = ({ reportsArray }) => {
     fullName: result.workers[0]?.fullName,
     office: result.workers[0]?.position === 'ENGINEER' ? 'INŻYNIER' : 'TECHNIK',
   }));
-  const mappedReportsElements = mappedReportsArray.map((report) => <Report {...report} />);
+
+  if (searchQuery) {
+    mappedReportsElements = mappedReportsElements.filter((report) => {
+      const isSearchResult =
+        report?.key?.toString?.().includes?.(searchQuery) ||
+        report?.date?.toString?.().includes?.(searchQuery) ||
+        report?.configuration?.toString?.().includes?.(searchQuery) ||
+        report?.fullName?.toString?.().includes?.(searchQuery) ||
+        report?.office?.toString?.().includes?.(searchQuery);
+      return isSearchResult;
+    });
+  }
+  const reportsToRender = mappedReportsElements.map((report) => <Report {...report} />);
+
+  let results =
+    reportsToRender.length === 0 ? (
+      <ErrorResults>
+        <Message messageType={'ERROR'} message={'Brak wyników'} />
+      </ErrorResults>
+    ) : (
+      <ReportsTable>
+        <ReportHeader />
+        <tbody>{reportsToRender}</tbody>
+      </ReportsTable>
+    );
 
   return (
     <>
-      <SearchBar label={'WYSZUKAJ'} placeholder={'Wpisz wyszukiwaną frazę'} />
-      <ReportsTable>
-        <ReportHeader />
-        <tbody>{mappedReportsElements}</tbody>
-      </ReportsTable>
+      <SearchBar
+        value={searchQuery}
+        setValue={(event) => setSearchQuery(event.target.value)}
+        label={'WYSZUKAJ'}
+        placeholder={'Wpisz wyszukiwaną frazę'}
+      />
+      {results}
     </>
   );
 };
