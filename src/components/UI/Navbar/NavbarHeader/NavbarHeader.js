513cdc373fee88fa39b2378a531285f530dd3e37
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-21 10:52:43 +0100

    FE-10 - Get rid of MENU sign in navbar

--- src/components/UI/Navbar/NavbarHeader/NavbarHeader.js
+++ src/components/UI/Navbar/NavbarHeader/NavbarHeader.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { StyledHamburger, StyledNavbarHeaderContainer, StyledPageTitle } from './StyledNavbarHeader';
+import { StyledHamburger, StyledNavbarHeaderContainer } from './StyledNavbarHeader';
 
 const NavbarHeader = ({ isOpen, onToggle, title }) => {
   return (
     <StyledNavbarHeaderContainer>
-      {isOpen ? <StyledPageTitle>{title}</StyledPageTitle> : <div />}
       <StyledHamburger color={'white'} isOpen={isOpen} menuClicked={onToggle} />
     </StyledNavbarHeaderContainer>
   );
